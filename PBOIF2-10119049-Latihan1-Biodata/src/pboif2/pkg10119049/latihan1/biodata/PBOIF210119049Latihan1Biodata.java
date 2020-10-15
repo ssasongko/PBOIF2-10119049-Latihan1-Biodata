51857aef46fe8a686f3de7079b9b4501845c1dc6
Nur Sasongko <32429721+ssasongko@users.noreply.github.com>
2020-10-15 21:09:15 +0700

    Add files via upload

---
+++ PBOIF2-10119049-Latihan1-Biodata/src/pboif2/pkg10119049/latihan1/biodata/PBOIF210119049Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119049.latihan1.biodata;
+
+/**
+ *
+ * @author 
+ * Nama  : Nur Sasongko
+ * Kelas : PBOIF2
+ * NIM   : 10119049
+ * Deskripsi Program : Program ini berisi program untuk menampilkan biodata
+ * 
+ */
+public class PBOIF210119049Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-----------------");
+        System.out.println("NAMA\t\t : Nur Sasongko ");
+        System.out.println("ALAMAT\t\t : Jalan Pak Gatot II No.19G ");
+        System.out.println("TEMPAT LAHIR\t : Bandung");
+        System.out.println("TANGGAL LAHIR\t : 16 Juli 2001");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t : Main Game");
+        System.out.println("Makes\t\t : Ayam Geprek");
+        System.out.println("Mikes\t\t : Susu");
+    }
+}
